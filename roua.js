4e75b62cd5f720929efe909cb889399e51482d55
GenMira <148327781+GenMira@users.noreply.github.com>
2023-10-21 22:34:24 +0900

    Update roua.js

--- roua.js
+++ roua.js
@@ -1,7 +1,8 @@
 var bt = document.getElementById("target");
 var bt = document.getElementById("bot");
 bt.addEventListener("click",function(e){
-    var numlist=["私","ウチ","僕","朕","俺","（自分の名前）","拙者","ワタクシ"];
+    var numlist=["私","ウチ","ウチ","ウチ","僕","僕","僕","朕","俺"
+    ,"（自分の名前）","（自分の名前）","拙者","ワタクシ","妾","ワタクシ","妾","自分"];
     var selectnum = Math.floor(Math.random()*numlist.length);
 
     var element = "<p>"+numlist[selectnum]+"</p>";
